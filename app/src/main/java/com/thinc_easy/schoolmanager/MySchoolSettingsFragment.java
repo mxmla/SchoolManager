fa9472a5d0ae5a224f67ee71147803d846c2d3cf
mxmla <maximilian.wassmann@gmail.com>
2016-07-07 14:59:46 +0200

    Version 2.0.15:
    - Fixed issue with saving and loading school preference

--- app/src/main/java/com/thinc_easy/schoolmanager/MySchoolSettingsFragment.java
+++ app/src/main/java/com/thinc_easy/schoolmanager/MySchoolSettingsFragment.java
@@ -2,6 +2,7 @@ package com.thinc_easy.schoolmanager;
 
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.provider.ContactsContract;
@@ -21,6 +22,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.internal.request.StringParcel;
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
 
@@ -39,9 +41,12 @@ public class MySchoolSettingsFragment extends Fragment {
     private Spinner sCountry, sSchool;
     private String countryName, countryID, schoolName, schoolID, prefKeySchoolID, prefKeyCountryID,
             prefKeySchoolName, prefKeyCountryName;
-    List<String> usaSchools, usaSchoolIDs, germanySchools, germanySchoolIDs;
     private String[][] allSchools;
-    private Button nitlButton;
+    private String[] countryNames, countryIDs;
+    private List<String> schools, schoolIDs;
+    private boolean foundCountryPreference;
+    int selCount;
+    private Button nitlButton, saveButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -69,21 +74,200 @@ public class MySchoolSettingsFragment extends Fragment {
         tvSchool.setTypeface(Typeface.createFromAsset(getResources().getAssets(), "Roboto-Medium.ttf"));
 
         nitlButton = (Button) v.findViewById(R.id.bNotInTheList);
+        saveButton = (Button) v.findViewById(R.id.bSaveSchoolSelection);
 
         initialSetup();
+        setUpSaveButton();
 
         return v;
     }
 
     @Override
     public void onPause(){
-        saveData();
+        //saveData();
         super.onPause();
     }
 
-    private void saveData(){
-        Toast.makeText(getActivity(), getActivity().getResources().getString(R.string.toast_saved), Toast.LENGTH_LONG).show();
 
+
+    private void initialSetup(){
+        countryNames = getActivity().getResources().getStringArray(R.array.my_school_countries);
+        final String countryIDs2Dim[][] = DataStorageHandler.toArray(getActivity(), "schools/Countries.txt", true);
+        countryIDs = new String[countryIDs2Dim.length+1];
+        countryIDs[0] = getActivity().getResources().getString(R.string.spinner_please_select);
+        for (int r1 = 0; r1 < countryIDs2Dim.length; r1++){
+            countryIDs[r1+1] = countryIDs2Dim[r1][0];
+        }
+
+        allSchools = DataStorageHandler.toArray(getActivity(), "schools/AllSchools.txt", true);
+
+
+        countryName = "[none]";
+        countryID = "[none]";
+        schoolName = "[none]";
+        schoolID = "[none]";
+
+
+        ArrayAdapter<String> adapterCountries = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, countryNames);
+        adapterCountries.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sCountry.setAdapter(adapterCountries);
+
+        selCount = 0;
+        sCountry.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id){
+                countryName = countryNames[parentView.getSelectedItemPosition()];
+                countryID = countryIDs[parentView.getSelectedItemPosition()];
+
+                if (selCount >= 1) setUpSchoolsSpinner(countryID, "oISListener");
+                selCount++;
+            }
+            public void onNothingSelected(AdapterView<?> parentView) {
+            }
+        });
+
+
+
+        foundCountryPreference = false;
+        if (prefs.contains(prefKeyCountryID)){
+            String prefCountryID = prefs.getString(prefKeyCountryID, "[none]");
+            for (int c = 0; c < countryIDs.length; c++){
+                if (countryIDs[c].equals(prefCountryID)){
+                    countryID = prefCountryID;
+                    countryName = countryNames[c];
+
+                    sCountry.setSelection(c, false);
+                    foundCountryPreference = true;
+                }
+            }
+        }
+
+        setUpSchoolsSpinner(countryID, "setUp");
+
+        if (foundCountryPreference){
+            if (prefs.contains(prefKeySchoolID)) {
+                final String prefSchoolID = prefs.getString(prefKeySchoolID, "[none]");
+                int position = 0;
+                for (int s = 0; s < schoolIDs.size(); s++) {
+                    if (prefSchoolID.equals(schoolIDs.get(s))) position = s;
+                }
+
+                if (schoolIDs.size() > 0){
+                    sSchool.setSelection(position, false);
+                }
+            }
+        }
+
+
+        setUpNotInTheListButton();
+
+    }
+
+    private void setUpSchoolsSpinner(String newCountryID, String caller){
+        schools = new ArrayList<>();
+        schools.add(getActivity().getResources().getString(R.string.spinner_please_select));
+        schoolIDs = new ArrayList<>();
+        schoolIDs.add(getActivity().getResources().getString(R.string.spinner_please_select));
+
+
+        for (int i = 0; i < allSchools.length; i++){
+            if (allSchools[i][0].startsWith(String.valueOf(newCountryID))){
+                schools.add(allSchools[i][1]);
+                schoolIDs.add(allSchools[i][0]);
+            }
+        }
+
+        //if (schoolIDs.size()>0) schoolID = schoolIDs.get(0);
+
+
+        ArrayAdapter<String> adapterSchools = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, schools);
+        adapterSchools.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sSchool.setAdapter(adapterSchools);
+        sSchool.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                schoolName = schools.get(parent.getSelectedItemPosition());
+                schoolID = schoolIDs.get(parent.getSelectedItemPosition());
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
+    private void schoolSpinnerFirstSetUp(String prefCountry){
+        setUpSchoolsSpinner(prefCountry, "ssFirstSetup");
+
+        List<String> schools = new ArrayList<>();
+        schools.add(getActivity().getResources().getString(R.string.spinner_please_select));
+        List <String> schoolIDs = new ArrayList<>();
+        schoolIDs.add(getActivity().getResources().getString(R.string.spinner_please_select));
+
+        for (int i = 0; i < allSchools.length; i++){
+            if (allSchools[i][0].startsWith(String.valueOf(prefCountry))){
+                schools.add(allSchools[i][1]);
+                schoolIDs.add(allSchools[i][0]);
+            }
+        }
+
+
+        if (prefs.contains(prefKeySchoolID)){
+            String prefSchoolID = prefs.getString(prefKeySchoolID, "[none]");
+            for (int s = 0; s < schoolIDs.size(); s++){
+                if (schoolIDs.get(s).equals(prefSchoolID)){
+                    schoolID = prefSchoolID;
+                    schoolName = schoolIDs.get(s);
+
+
+                    final List<String> fSchools = schools;
+                    final List<String> fSchoolIDs = schoolIDs;
+
+                    ArrayAdapter<String> adapterSchools = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, fSchools);
+                    adapterSchools.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    sSchool.setAdapter(adapterSchools);
+                    sSchool.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            schoolName = fSchools.get(parent.getSelectedItemPosition());
+                            schoolID = fSchoolIDs.get(parent.getSelectedItemPosition());
+                        }
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+                        }
+                    });
+
+                    sSchool.setSelection(s, true);
+                }
+            }
+        }
+    }
+
+    private void setUpNotInTheListButton(){
+        nitlButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MySchoolSettingsActivity) getActivity()).showAddSchoolFragment();
+            }
+        });
+    }
+
+    private void setUpSaveButton(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            saveButton.setBackgroundTintList(getActivity().getResources().getColorStateList(R.color.button_color_state_list));
+            saveButton.setTextColor(getActivity().getResources().getColor(R.color.TextDarkBg));
+        } else {
+            saveButton.setTextColor(getActivity().getResources().getColor(R.color.colorAccent));
+        }
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveData();
+            }
+        });
+    }
+
+    private void saveData(){
+        Toast.makeText(getActivity(), getActivity().getResources().getString(R.string.toast_saved), Toast.LENGTH_SHORT).show();
 
         if (prefs.contains(prefKeySchoolID) && !prefs.getString(prefKeySchoolID, "-").equals(schoolID)
                 && prefs.contains(prefKeyCountryID)){
@@ -100,141 +284,11 @@ public class MySchoolSettingsFragment extends Fragment {
                     .build());
         }
 
+
         prefs.edit().putString(prefKeyCountryID, countryID).apply();
         prefs.edit().putString(prefKeySchoolID, schoolID).apply();
         prefs.edit().putString(prefKeyCountryName, countryName).apply();
         prefs.edit().putString(prefKeySchoolName, schoolName).apply();
-    }
-
-    private void initialSetup(){
-        final String usa = getActivity().getResources().getString(R.string.country_united_states_of_america);
-        final String germany = getActivity().getResources().getString(R.string.country_germany);
-        final String[] countries = {getActivity().getResources().getString(R.string.spinner_please_select), usa, germany};
-        final String[] countryIDs = {getActivity().getResources().getString(R.string.spinner_please_select), "0101", "0201"};
-
-        usaSchools = new ArrayList<>();
-        usaSchoolIDs = new ArrayList<>();
-        germanySchools = new ArrayList<>();
-        germanySchoolIDs = new ArrayList<>();
-
-        usaSchools.add(getActivity().getResources().getString(R.string.spinner_please_select));
-        usaSchoolIDs.add(getActivity().getResources().getString(R.string.spinner_please_select));
-        germanySchools.add(getActivity().getResources().getString(R.string.spinner_please_select));
-        germanySchoolIDs.add(getActivity().getResources().getString(R.string.spinner_please_select));
-
-        allSchools = DataStorageHandler.toArray(getActivity(), "schools/AllSchools.txt", true);
-
-        for (int i = 0; i < allSchools.length; i++){
-            if (allSchools[i][0].startsWith("0101")){
-                usaSchools.add(allSchools[i][1]);
-                usaSchoolIDs.add(allSchools[i][0]);
-            } else if (allSchools[i][0].startsWith("0201")){
-                germanySchools.add(allSchools[i][1]);
-                germanySchoolIDs.add(allSchools[i][0]);
-            }
-        }
-
-        countryName = getActivity().getResources().getString(R.string.country_united_states_of_america);
-        countryID = "0101";
-        schoolName = "[none]";
-        schoolID = "[none]";
-
-
-        ArrayAdapter<String> adapterCountries = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, countries);
-        adapterCountries.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        sCountry.setAdapter(adapterCountries);
-        sCountry.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id){
-                countryName = countries[parentView.getSelectedItemPosition()];
-                countryID = countryIDs[parentView.getSelectedItemPosition()];
-                setUpSchoolsSpinner(countryID, "[none]");
-            }
-            public void onNothingSelected(AdapterView<?> parentView) {
-            }
-        });
-
-        boolean schoolsSpinnerSetUp = false;
-        if (prefs.contains(prefKeyCountryID)){
-            String prefCountryID = prefs.getString(prefKeyCountryID, "[none]");
-            for (int c = 0; c < countryIDs.length; c++){
-                if (countryIDs[c].equals(prefCountryID)){
-                    countryID = prefCountryID;
-                    countryName = countries[c];
-
-                    sCountry.setSelection(c);
-
-                    if (prefs.contains(prefKeySchoolID)){
-                        String prefSchoolID = prefs.getString(prefKeySchoolID, "[none]");
-                        for (int s = 0; s < allSchools.length; s++){
-                            if (allSchools[s][0].equals(prefSchoolID)){
-                                schoolID = prefSchoolID;
-                                schoolName = allSchools[s][1];
-
-                                setUpSchoolsSpinner(countryID, schoolID);
-                                schoolsSpinnerSetUp = true;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        if (!schoolsSpinnerSetUp) setUpSchoolsSpinner(countryID, "[none]");
-
-        setUpNotInTheListButton();
-
-    }
-
-    private void setUpSchoolsSpinner(String countryID, String selectedSchoolID){
-        List<String> schools;
-        List<String> schoolIDs;
-        if (countryID.equals("0101")){
-            schools = usaSchools;
-            schoolIDs = usaSchoolIDs;
-        } else if (countryID.equals("0201")) {
-            schools = germanySchools;
-            schoolIDs = germanySchoolIDs;
-        } else {
-            schools = new ArrayList<>();
-            schools.add(getActivity().getResources().getString(R.string.spinner_please_select));
-            schoolIDs = new ArrayList<>();
-            schoolIDs.add(getActivity().getResources().getString(R.string.spinner_please_select));
-        }
-
-        if (schoolIDs.size()>0) schoolID = schoolIDs.get(0);
-
-        final List<String> fSchools = schools;
-        final List<String> fSchoolIDs = schoolIDs;
-
-
-        ArrayAdapter<String> adapterSchools = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, fSchools);
-        adapterSchools.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        sSchool.setAdapter(adapterSchools);
-        sSchool.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                schoolName = fSchools.get(parent.getSelectedItemPosition());
-                schoolID = fSchoolIDs.get(parent.getSelectedItemPosition());
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
-
-        if (selectedSchoolID != null && !selectedSchoolID.equals("[none]") && !selectedSchoolID.equals("")){
-            for (int i = 0; i < fSchoolIDs.size(); i++){
-                if (fSchoolIDs.get(i).equals(selectedSchoolID)) sSchool.setSelection(i);
-            }
-        }
-    }
-
-    private void setUpNotInTheListButton(){
-        nitlButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ((MySchoolSettingsActivity) getActivity()).showAddSchoolFragment();
-            }
-        });
     }
 
     @Override
