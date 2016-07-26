44d617424776bb71d3e4150515fff72b7d09cb7a
mxmla <maximilian.wassmann@gmail.com>
2016-07-26 13:29:42 +0200

    Version 2.0.25:
    - Added 5 new schools (USA, Germany, Australia, Madagaskar)

--- app/src/main/java/com/thinc_easy/schoolmanager/MySchoolFragment.java
+++ app/src/main/java/com/thinc_easy/schoolmanager/MySchoolFragment.java
@@ -103,8 +103,9 @@ public class MySchoolFragment extends Fragment {
                         */
 
                         webView.loadUrl(url);
+                        //webView.loadData(url, "text/html; charset=UTF-8", null);
                         foundURL = true;
-                        Toast.makeText(getActivity(), "Loading...", Toast.LENGTH_LONG).show();
+                        Toast.makeText(getActivity(), "Loading url..."+url, Toast.LENGTH_LONG).show();
                     }
                 }
             }
